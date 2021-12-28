dd925e50a2848a321945b4d68156cc8e6dd0fb49
cdh616x <91150236+cdh616x@users.noreply.github.com>
2021-12-28 14:15:56 -0500

    changed logic to correctly display appropriate sign at appropriate times

--- sign.js
+++ sign.js
@@ -4,9 +4,9 @@ let time = date.getHours();
 let year = date.getYear();
 
 function sign() {
-  if ((day === 1 || 2 || 3 || 4 || 5) && (time <= 10 || time >= 19)) {
+  if ((day === 1 || 2 || 3 || 4 || 5) && (time < 10 || time >= 19)) {
     $("#sign").attr("src", "images/closed.jpeg");
-  } else if ((day === 6 || 0) && (time <= 11 || time >= 16)) {
+  } else if ((day === 6 || 0) && (time < 11 || time >= 16)) {
     $("#sign").attr("src", "images/closed.jpeg");
   }
 }
